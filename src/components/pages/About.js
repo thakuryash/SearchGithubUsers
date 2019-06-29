66c1d7043f6082edc42232e9d4acb18b4f66d520
Yash <thakuryash@live.com>
2019-06-29 19:24:48 -0400

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const About = props => {
+  return (
+    <Fragment>
+      <h1>ABout this App</h1>
+      <p>App to search Github users</p>
+      <p>Version 3.3.3</p>
+    </Fragment>
+  );
+};
+
+About.propTypes = {};
+
+export default About;
---
+++ src/components/users/Users.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import Useritem from "./Useritem";
+import Spinner from "../layout/Spinner";
+import GithubContext from "../../context/github/githubContext";
+
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <Useritem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gripGap: "1rem",
+  color: "red"
+};
+
+export default Users;
